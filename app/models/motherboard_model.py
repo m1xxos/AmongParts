6232e84a0139b25ddde14b3cb138ee005f264e80
dyuguytjdr <66390094+m1xxos@users.noreply.github.com>
2022-03-22 20:50:53 +0300

    Update motherboard_model.py

--- app/models/motherboard_model.py
+++ app/models/motherboard_model.py
@@ -5,44 +5,45 @@ from typing import Optional, List
 
 
 class MotherBoard(BaseModel):
-    name: str
-    price: int = Query(..., ge=1)
-    brand: str
-    type: str
-    socket: str
-    form_factor: str
-    chipset: str
-    memory_type: str
-    memory_slots: int
-    memory_speed: int
-    memory_maximum: int
-    memory_channel: int
-    pci_2: Optional[int]
-    pci_3: Optional[int]
-    pci_4: Optional[int]
-    pci_5: Optional[int]
-    audio_chip: str
-    audio_channel: str
-    sata_amount: int
-    m_2_amount: int
+    name: str = None
+    price: int = None
+    brand: str = None
+    type: str = None
+    photos: List[str] = None
+    links: List[str] = None
+    availability: bool = None
+    socket: str = None
+    form_factor: str = None
+    chipset: str = None
+    memory_type: str = None
+    memory_slots: int = None
+    memory_speed: str = None
+    memory_maximum: str = None
+    memory_channel: str = None
+    pci_2: Optional[int] = None
+    pci_3: Optional[int] = None
+    pci_4: Optional[int] = None
+    pci_5: Optional[int] = None
+    audio_chip: Optional[str] = None
+    audio_channel: Optional[str] = None
+    sata_amount: int = None
+    m_2_amount: Optional[int] = None
     raid: Optional[bool] = None
     optane: Optional[bool] = None
-    ethernet: str
-    slot_ps2: Optional[int] = None
+    ethernet: str = None
+    slot_ps2: Optional[str] = None
     slot_usb2: Optional[int] = None
     slot_usb3: Optional[int] = None
     slot_usbc: Optional[int] = None
     slot_display_port: Optional[int] = None
     slot_vga: Optional[int] = None
-    slot_com: Optional[int] = None
-    slot_lpt: Optional[int] = None
+    slot_dvi: Optional[int] = None
     slot_hdmi: Optional[int] = None
     slot_thunderbolt: Optional[int] = None
     wi_fi: Optional[bool] = None
     bluetooth: Optional[bool] = None
     sli: Optional[str] = None
-    power: str
-    photos: List[str] = None
+    power: str = None
 
 
 @dataclass
