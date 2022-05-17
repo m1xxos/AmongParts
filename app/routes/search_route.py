02632d4969ac261b281c7570f7b829ee90df8269
m1xxos <66390094+m1xxos@users.noreply.github.com>
2022-05-17 20:44:11 +0300

    added skip to search

--- app/routes/search_route.py
+++ app/routes/search_route.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio
 
-from app.globals import DEFAULT_LIMIT, router
+from app.globals import DEFAULT_LIMIT, router, DEFAULT_SKIP
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     "mongodb+srv://m1xxos:vn9OAsrfRrCEMSmx@amongpartscluster.jbzts.mongodb.net/test?authSource=admin&replicaSet=atlas"
@@ -9,7 +9,7 @@ database = client.AmongParts
 
 
 @router.get("/search", tags=["Search"])
-async def search_all(name: str, limit: int = DEFAULT_LIMIT):
+async def search_all(name: str, limit: int = DEFAULT_LIMIT, skip: int = DEFAULT_SKIP):
     pipeline = [
         {
             "$search": {
@@ -20,6 +20,7 @@ async def search_all(name: str, limit: int = DEFAULT_LIMIT):
                 }
             }
         },
+        {"$skip": skip},
         {"$limit": limit},
         {
             '$project': {
@@ -27,15 +28,38 @@ async def search_all(name: str, limit: int = DEFAULT_LIMIT):
             }
         }
     ]
+    pipeline_count = [
+        {
+            '$search': {
+                'index': 'summary_search',
+                'text': {
+                    'query': name,
+                    'path': 'name'
+                }
+            }
+        },
+        {
+            '$count': 'amount'
+        }
+    ]
+
     cursor = database["summary"].aggregate(pipeline)
     results = []
     async for res in cursor:
         results.append(res)
-    return results
+
+    cursor = database["summary"].aggregate(pipeline_count)
+    amount = await cursor.to_list(1)
+    if amount:
+        amount = amount[0]['amount']
+    else:
+        amount = 0
+
+    return {"amount": amount, "data": results}
 
 
 @router.get("/search/{category}", tags=["Search"])
-async def search_all(category: str, name: str, limit: int = DEFAULT_LIMIT):
+async def search_all(category: str, name: str, limit: int = DEFAULT_LIMIT, skip: int = DEFAULT_SKIP):
     pipeline = [
         {
             '$search': {
@@ -49,15 +73,41 @@ async def search_all(category: str, name: str, limit: int = DEFAULT_LIMIT):
             '$match': {
                 'category': category
             }
-        }, {"$limit": limit},
+        }, {"$skip": skip},
+        {"$limit": limit},
         {
             '$project': {
                 '_id': 0
             }
         }
     ]
+    pipeline_count = [
+        {
+            '$search': {
+                'index': 'summary_search',
+                'text': {
+                    'query': name,
+                    'path': 'name'
+                }
+            }
+        },{
+            '$match': {
+                'category': category
+            }
+        },
+        {
+            '$count': 'amount'
+        }
+    ]
     cursor = database["summary"].aggregate(pipeline)
     results = []
     async for res in cursor:
         results.append(res)
-    return results
+
+    cursor = database["summary"].aggregate(pipeline_count)
+    amount = await cursor.to_list(1)
+    if amount:
+        amount = amount[0]['amount']
+    else:
+        amount = 0
+    return {"amount": amount, "data": results}
